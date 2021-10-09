91d30f603dce50d8ecb1fa80c9388b0a17b9755e
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-09 13:40:57 +0530

    Add compare page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import TopBar from "./components/TopBar/TopBar";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
     </div>
   );
 }
---
+++ client/src/pages/ComparePage/Compare.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Compare.css";
+
+function Compare() {
+  return <div>This is Compare page</div>;
+}
+
+export default Compare;
