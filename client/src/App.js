132a8496ddbb7592a639d092a145b0d1c421932c
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-09 15:36:10 +0530

    Compare: Created Ui of Compare.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
+import { AppBar, Button, Toolbar } from "@material-ui/core";
 import "./App.css";
-import TopBar from "./components/TopBar/TopBar";
-import Home from "./pages/HomePage/Home";
+import Compare from "./pages/ComparePage/Compare";
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
-      <Home />
+      <AppBar position="static">
+        <Toolbar>
+          <div className="topbar">
+            <div className="leftButtons">
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">Compare</Button>
+              <Button color="inherit">Educational Videos</Button>
+              <Button color="inherit">Quiz</Button>
+              <Button color="inherit">Game</Button>
+            </div>
+            <div className="rightButtons">
+              <Button color="inherit">Login</Button>
+              <Button color="inherit">Register</Button>
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Compare />
     </div>
   );
 }
--- client/src/pages/ComparePage/Compare.js
+++ client/src/pages/ComparePage/Compare.js
@@ -1,8 +1,86 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Input,
+  InputLabel,
+} from "@material-ui/core";
+import React, { useState } from "react";
 import "./Compare.css";
 
 function Compare() {
-  return <div>This is Compare page</div>;
+  const [page, setPage] = useState(1);
+
+  const openMonthly = () => {
+    setPage(1);
+  };
+
+  const openLumpSum = () => {
+    setPage(0);
+  };
+
+  return (
+    <div className="container">
+      <Card className="root" variant="outlined">
+        <div className="upButtons">
+          <Button onClick={openMonthly}>Monthly investemt</Button>
+          <Button onClick={openLumpSum}>Lump Sum</Button>
+        </div>
+
+        {page ? (
+          // Monthly Investment
+          <CardContent>
+            <FormControl>
+              <InputLabel>Monthly Investment Amount (Rs)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <FormControl>
+              <InputLabel>Investment Period (years)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <FormControl>
+              <InputLabel>Expected Return (%)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <Button> Calculate </Button>
+            <Box>
+              Total Amount <br />
+              Invested Amount
+            </Box>
+          </CardContent>
+        ) : (
+          // Lump Sum
+          <CardContent>
+            <FormControl>
+              <InputLabel>Investment Amount (Rs)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <FormControl>
+              <InputLabel>Investment Period (years)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <FormControl>
+              <InputLabel>Expected Return (%)</InputLabel>
+              <Input></Input>
+            </FormControl>
+
+            <Button> Calculate </Button>
+            <Box>
+              Total Amount <br />
+              Invested Amount
+            </Box>
+          </CardContent>
+        )}
+      </Card>
+    </div>
+  );
 }
 
 export default Compare;
