48bbbfb6ef97aaacb8a86757d75bede40e2ab4bb
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-09 18:30:31 +0530

    Create UI of compare page.

--- client/src/pages/ComparePage/Invsnon.js
+++ client/src/pages/ComparePage/Invsnon.js
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography,
+} from "@material-ui/core";
+import "./Compare.css";
 
 function Invsnon() {
-  return <div>This is Invested Vs not invested</div>;
+  return (
+    <div>
+      <Typography>
+        <h1>What if you invested x Rs vs not invested that x Rs</h1>
+      </Typography>
+      <Card class="root" variant="outlined">
+        <CardContent class="content">
+          <FormControl>
+            <InputLabel>You invested (Rs)</InputLabel>
+            <Input></Input>
+          </FormControl>
+          <FormControl>
+            <InputLabel>Investment period (years)</InputLabel>
+            <Input></Input>
+          </FormControl>
+          <FormControl>
+            <InputLabel>Expected Return (%)</InputLabel>
+            <Input></Input>
+          </FormControl>
+          <Button className="calcbutton">Calculate</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default Invsnon;
